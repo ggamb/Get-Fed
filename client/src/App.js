9c72b1458e8f28d57284517e254694fe59bff80c
ggamb <glenngambardella372@gmail.com>
2022-01-14 14:17:02 -0500

    added api fetch call and add results to array in Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Signup from './pages/Signup';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -40,28 +41,6 @@ const client = new ApolloClient({
 
 function App() {
 
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
-  function success(pos) {
-    var crd = pos.coords;
-
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-  }
-
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
-
-  navigator.geolocation.getCurrentPosition(success, error, options);
-
-  
   return (
     <ApolloProvider client={client}>
       <Router>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,67 @@
 import React from "react";
 
 const Home = () => {
+
+    let restaurantsArray = []
+
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+
+        /*fetch(`https://api.documenu.com/v2/restaurants/search/geo?lat=${crd.latitude}&lon=${crd.longitude}&distance=20&size=30&page=1&fullmenu=true&top_cuisines=false`, {
+            "method": "GET",
+            "headers": {
+                "x-api-key": "63f52417403d221ebe783f211c6d2324"
+            }
+        })
+        .then(response => response.json())
+        .then(restaurantData => {
+            console.log(restaurantData);
+            restaurantData.data.forEach(restaurant => {
+                restaurantsArray.push(restaurant)
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });*/
+    }
+
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+    console.log('restraurants array', restaurantsArray);
+
+
     return (
+
         <div className="container">
-            <h1>Hello world!</h1>
+            <div className="my-2">
+                <h2>Our Products:</h2>
+                {state.products.length ? (
+                    <div className="flex-row">
+                        {restaurantsArray.map((restaurant) => (
+
+                        ))}
+                    </div>
+                ) : (
+                    <h3>Please enter your location or accept geolocation permissions!</h3>
+                )}
+                {loading ? <img src={spinner} alt="loading" /> : null}
+            </div>
         </div>
     );
 };
