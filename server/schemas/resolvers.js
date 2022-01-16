3f491025d569c044e6cc2a4dc75d026f7aad2d0d
tygrski <77369211+tygrski@users.noreply.github.com>
2022-01-15 19:21:39 -0600

    query order working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,10 +6,10 @@ const resolvers = {
     Query: {
         user: async (parent, args, context) => {
             if (context.user) {
-                const user = await User.findById(context.user._id)/*.populate({
+                const user = await User.findById(context.user._id).populate({
                     path: 'orders.products',
                     populate: 'category'
-                })*/;
+                });
 
                 //user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
