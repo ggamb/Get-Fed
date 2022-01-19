155758c86efc2532dab30ff13972680ce54e00a7
ggamb <glenngambardella372@gmail.com>
2022-01-19 10:29:44 -0500

    updated package.json for deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/teamCuisine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/teamCuisine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
